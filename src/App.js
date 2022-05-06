583f5d3f0afb938e404bb2172b071eed4de5d00f
Bruno Zamorano Alves Monteiro <bzam1204@gmail.com>
2022-05-05 21:35:26 -0400

    Inserção de manuais-teste, afim de que possamos ter a primeira experiência de uso da ferramenta

--- src/App.js
+++ src/App.js
@@ -3,20 +3,6 @@ import styled from "styled-components";
 import MainContainer from "./Conponents/MainContainer";
 
 function App() {
-  const Title = styled.h1`
-    text-align: center;
-  `;
-
-  const RepoName = styled.strong`
-    background: linear-gradient(
-      90deg,
-      rgba(2, 0, 36, 1) 0%,
-      rgba(9, 9, 121, 1) 35%,
-      rgba(0, 212, 255, 1) 100%
-    );
-    -webkit-background-clip: text;
-    -webkit-text-fill-color: transparent;
-  `;
   return (
     <>
       <MainContainer />
--- src/api/posts.js
+++ src/api/posts.js
@@ -16,6 +16,50 @@ const posts = [
       { paragraph: "Agora, você pode selecionar a frequência de salvamento." },
     ],
   },
+  {
+    id: 2,
+    category: "Libre Office",
+    author: "Bruno Zamorano",
+    title: "Autosave no Libre Office",
+    body: [
+      {
+        paragraph:
+          "Você pode encontrar as opções de salvamento automático em:  Ferramentas ▸ Opções ▸ Load/Save ▸ General, ou em Mac OSX, em “Preferências”. Como na imagem a segui:",
+      },
+      {
+        image:
+          "https://ask.libreoffice.org/uploads/asklibo/original/3X/5/c/5cb7c1564fd2176d6f6b0998c41ee625c0b1fa28.png",
+      },
+      { paragraph: "Agora, você pode selecionar a frequência de salvamento." },
+    ],
+  },
+  {
+    id: 3,
+    category: "Display",
+    author: "Bruno Zamorano",
+    title: "Alterar a Resolução dos Monitores",
+    body: [
+      {
+        paragraph:
+          "Na Área de trabalho, clique com o botão direito para mostrar o menu de contexto.",
+      },
+      {
+        paragraph: "Após isso, Clique na opção 'Configurações de exibição'.",
+      },
+      {
+        paragraph:
+          "Então, uma pequena janela será aberta com algumas configurações de vídeo. Aqui você pode aumentar o tamanho da sua tela, clicando em “Resolução de tela”. Ah! E seu computador sempre vai sugerir um tamanho recomendado para cada tela/monitor.",
+      },
+      {
+        image:
+          "https://t.ctcdn.com.br/1YJXfvU1gHxsJB3yxolV7u8otbA=/660x0/smart/i544948.png",
+      },
+      {
+        paragraph:
+          "Agora, você pode selecionar a resolução mais adequada ao monitor.",
+      },
+    ],
+  },
 ];
 
 export default posts;
